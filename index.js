2a0548d0a15ecb15d3a631c8667408f0b3b31eb9
Jon Borgonia <jon@gomagames.com>
2018-10-26 17:52:00 -0700

    p2pwn /disconnect with hostId

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const P2PWN = process.env.P2PWN || 'https://p2pwithme.2018.nodeknockout.com';
 // CHANGE ME
 const appName = 'p2pwnme-ready-node-wss';
 
+let p2pwn = {
+  hostId : null
+};
+
 const tunnel = localtunnel(PORT, (err, { url }) => {
   if (err) {
     console.error(err);
@@ -23,15 +27,20 @@ const tunnel = localtunnel(PORT, (err, { url }) => {
     },
     json: true
   })
-  .then(console.log)
+  .then(res => p2pwn = res)
   .catch(({message}) => console.error(message))
 });
 
 tunnel.on('close', function() {
   console.log('Tunnel closed!');
-  rp(`${P2PWN}/disconnect`)
-    .then(console.log)
-    .catch(({message}) => console.error(message))
+  rp({
+    uri: `${P2PWN}/host/${p2pwn.hostId}/disconnect`,
+    method: 'POST',
+    body: {},
+    json: true
+  })
+  .then(console.log)
+  .catch(({message}) => console.error(message))
 });
 
 server.listen(PORT, () => console.log(`WSS Listening on port ${PORT}`));
