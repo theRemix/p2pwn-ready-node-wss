264cc0daab7e90b2bdf62ce595c1120493b8bbd8
Jon Borgonia <jon@gomagames.com>
2018-10-26 17:48:59 -0700

    send appName and url to P2PWN service

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const localtunnel = require('localtunnel');
 const PORT = process.env.PORT || 3000;
 const P2PWN = process.env.P2PWN || 'https://p2pwithme.2018.nodeknockout.com';
 
-const tunnel = localtunnel(PORT, function(err, tunnel) {
+// CHANGE ME
+const appName = 'p2pwnme-ready-node-wss';
+
+const tunnel = localtunnel(PORT, (err, { url }) => {
   if (err) {
     console.error(err);
     return process.exit(1);
   }
 
-  console.log(`Tunnel created at: ${tunnel.url}`);
-  rp(`${P2PWN}/connect`)
-    .then(console.log)
-    .catch(({message}) => console.error(message))
+  console.log(`Tunnel created at: ${url}`);
+  rp({
+    uri: `${P2PWN}/connect`,
+    method: 'POST',
+    body: {
+      appName,
+      url
+    },
+    json: true
+  })
+  .then(console.log)
+  .catch(({message}) => console.error(message))
 });
 
 tunnel.on('close', function() {
