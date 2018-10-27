cfbb4d669067d0aff782f19a4a92c9b600be4cda
Jon Borgonia <jon@gomagames.com>
2018-10-26 17:43:36 -0700

    add localtunnel
    - to report /connect and /disconnect to P2PWN

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const server = require('./wss');
+const rp = require('request-promise');
+const localtunnel = require('localtunnel');
 const PORT = process.env.PORT || 3000;
+const P2PWN = process.env.P2PWN || 'https://p2pwithme.2018.nodeknockout.com';
+
+const tunnel = localtunnel(PORT, function(err, tunnel) {
+  if (err) {
+    console.error(err);
+    return process.exit(1);
+  }
+
+  console.log(`Tunnel created at: ${tunnel.url}`);
+  rp(`${P2PWN}/connect`)
+    .then(console.log)
+    .catch(({message}) => console.error(message))
+});
+
+tunnel.on('close', function() {
+  console.log('Tunnel closed!');
+  rp(`${P2PWN}/disconnect`)
+    .then(console.log)
+    .catch(({message}) => console.error(message))
+});
 
 server.listen(PORT, () => console.log(`WSS Listening on port ${PORT}`));
--- wss.js
+++ wss.js
@@ -12,4 +12,5 @@ wss.on('connection', function connection(ws) {
   ws.send('connected');
 });
 
+
 module.exports = server;
